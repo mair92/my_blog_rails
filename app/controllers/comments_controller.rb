2e07e82f92b1bdf6f89ad23f161b6b72cb6d38e1
mairbekaushev <Mair>
2020-05-16 16:14:08 +0300

    Permit username for signin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
 
     redirect_to article_path(@article)
   end 
-
+ 
   private 
   def comment_params
     params.require(:comment).permit(:author, :body)  
