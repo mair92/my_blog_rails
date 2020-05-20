713649012964f6f5479066370d2bf308ec1fb2fa
mairbekaushev <Mair>
2020-05-21 01:37:38 +0300

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -18,7 +18,16 @@ describe Article do
       article = create(:article, title: 'Lorem ipsum')
       # assert, проверка
       expect(article.subject).to eq 'Lorem ipsum'
+    end
+  end
 
+  describe "#last_comment" do
+    it "returns the last comment" do
+      #создаем статью, но в этот раз с комментариями э
+      article = create(:article_with_comments)
+
+      # проверка
+      expect(article.last_comment.body).to eq "comment body 3"
     end
   end
 end
