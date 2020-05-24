1ee3f60a04af51909e58987538b0bc4b952f41da
mairbekaushev <Mair>
2020-05-24 20:34:22 +0300

    refactoring session_helper.rb

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,8 +1,9 @@
 def sign_up
     visit new_user_registration_path
-    fill_in :user_email, with: 'user123@example.com'
-    fill_in :user_username, with: 'user123'
-    fill_in :user_password, with: 'user1234'
-    fill_in :user_password_confirmation, with: 'user1234'
-    click_button 'commit'
+    fill_in :user_email, with: 'user1234@example.com'
+    fill_in :user_username, with: 'userrrrr'
+    fill_in :user_password, with: 'user12345'
+    fill_in :user_password_confirmation, with: 'user12345'
+    click_button 'Sign up'
 end
+
