f1d1389322435244a8f432aec72f6ca7e789b15a
mairbekaushev <Mair>
2020-05-24 20:33:36 +0300

    refactoring visitor creates account page

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -1,8 +1,11 @@
 require "rails_helper"
 
 feature  "Account creation" do
-  scenario "allows guest to create account" do 
+  before(:each) do
     sign_up
+  end
+  scenario "allows guest to create account" do 
+   # sign_up
     expect(page).to have_content I18n.t 'devise.registrations.signed_up'
   end  
 end
