1d5319d2091189c5cf0bf78403a99f2418cdc00a
mairbekaushev <Mair>
2020-05-29 20:25:53 +0300

    Add user_id to article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_121545) do
+ActiveRecord::Schema.define(version: 2020_05_29_150046) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
