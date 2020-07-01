a7f53605075dd2dfae4058d7a1cbc35425abb1d9
Mair <aushev92@gmail.com>
2020-07-01 23:16:58 +0300

    add username to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,9 @@ class CommentsController < ApplicationController
 
   def create 
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
-    @comment = @article.comments.last
+    @comment = @article.comments.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
-
     redirect_to article_path(@article)
   end 
  
